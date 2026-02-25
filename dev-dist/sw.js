b1a445acd944eaad9050009c4436e1b98cad02bc
DevGabriel0402 <gabriel.profissional01@gmail.com>
2026-02-24 23:52:46 -0300

    Atualizando SW

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hmcilp3166g"
+    "revision": "0.quc2aprd9t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
